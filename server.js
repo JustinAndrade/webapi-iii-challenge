94cf367fd4bfc76234733b24da605ac22485c7a9
JustinAndrade <j.andrade.john@gmail.com>
2019-05-14 02:56:44 -0400

    server listening

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const express = 'express';
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+	res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
-
-module.exports = server;
+	console.log(`${req.method} Request`);
+	next();
+}
